3e15766e1c310411a85cc78f0df1ade7ead5682e
TheRedstoneRadiant <ibrahimeatspizza@gmail.com>
2022-04-19 14:44:50 -0400

    unindent

--- public/js/camera.js
+++ public/js/camera.js
@@ -1,78 +1,78 @@
-    // I skidded all of this code. ALL OF IT
+  // I skidded all of this code. ALL OF IT
 
-    const zoomElement = document.getElementById("zoom-container");
-    let zoom = 1;
-    const ZOOM_SPEED = 0.1;
+  const zoomElement = document.getElementById("zoom-container");
+  let zoom = 1;
+  const ZOOM_SPEED = 0.1;
 
-    document.addEventListener("wheel", function(e) {
+  document.addEventListener("wheel", function(e) {
 
-        if (e.deltaY < 0) {
-            zoomElement.style.transform = `scale(${zoom += ZOOM_SPEED})`;
-        } else {
-            zoomElement.style.transform = `scale(${zoom -= ZOOM_SPEED})`;
-        }
+      if (e.deltaY < 0) {
+          zoomElement.style.transform = `scale(${zoom += ZOOM_SPEED})`;
+      } else {
+          zoomElement.style.transform = `scale(${zoom -= ZOOM_SPEED})`;
+      }
 
-    });
-
-
-
-    var dragItem = document.getElementById("board");
-    var container = document.getElementById("container");
+  });
 
 
-    var active = false;
-    var currentX;
-    var currentY;
-    var initialX;
-    var initialY;
-    var xOffset = 0;
-    var yOffset = 0;
 
-    container.addEventListener("touchstart", dragStart, false);
-    container.addEventListener("touchend", dragEnd, false);
-    container.addEventListener("touchmove", drag, false);
+  var dragItem = document.getElementById("board");
+  var container = document.getElementById("container");
 
-    container.addEventListener("mousedown", dragStart, false);
-    container.addEventListener("mouseup", dragEnd, false);
-    container.addEventListener("mousemove", drag, false);
 
-    function dragStart(e) {
-        if (e.type === "touchstart") {
-            initialX = e.touches[0].clientX - xOffset;
-            initialY = e.touches[0].clientY - yOffset;
-        } else {
-            initialX = e.clientX - xOffset;
-            initialY = e.clientY - yOffset;
-        }
+  var active = false;
+  var currentX;
+  var currentY;
+  var initialX;
+  var initialY;
+  var xOffset = 0;
+  var yOffset = 0;
 
-        if (e.target === dragItem) {
-            active = true;
-        }
-    }
+  container.addEventListener("touchstart", dragStart, false);
+  container.addEventListener("touchend", dragEnd, false);
+  container.addEventListener("touchmove", drag, false);
 
-    function dragEnd(e) {
-        initialX = currentX;
-        initialY = currentY;
+  container.addEventListener("mousedown", dragStart, false);
+  container.addEventListener("mouseup", dragEnd, false);
+  container.addEventListener("mousemove", drag, false);
 
-        active = false;
-    }
+  function dragStart(e) {
+      if (e.type === "touchstart") {
+          initialX = e.touches[0].clientX - xOffset;
+          initialY = e.touches[0].clientY - yOffset;
+      } else {
+          initialX = e.clientX - xOffset;
+          initialY = e.clientY - yOffset;
+      }
 
-    function drag(e) {
-        if (active) {
+      if (e.target === dragItem) {
+          active = true;
+      }
+  }
 
-            e.preventDefault();
+  function dragEnd(e) {
+      initialX = currentX;
+      initialY = currentY;
 
-            if (e.type === "touchmove") {
-                currentX = e.touches[0].clientX - initialX;
-                currentY = e.touches[0].clientY - initialY;
-            } else {
-                currentX = e.clientX - initialX;
-                currentY = e.clientY - initialY;
-            }
+      active = false;
+  }
 
-            xOffset = currentX;
-            yOffset = currentY;
+  function drag(e) {
+      if (active) {
 
-            dragItem.style.transform = `translate3d(${currentX}px, ${currentY}px, 0)`;
-        }
-    }
+          e.preventDefault();
+
+          if (e.type === "touchmove") {
+              currentX = e.touches[0].clientX - initialX;
+              currentY = e.touches[0].clientY - initialY;
+          } else {
+              currentX = e.clientX - initialX;
+              currentY = e.clientY - initialY;
+          }
+
+          xOffset = currentX;
+          yOffset = currentY;
+
+          dragItem.style.transform = `translate3d(${currentX}px, ${currentY}px, 0)`;
+      }
+  }
